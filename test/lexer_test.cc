228a8fe0cb006a6174a2fe817587f492caa01a9f
xiebei <xiebei1108@outlook.com>
2019-06-01 00:10:22 +0800

    feat: lexer test

--- test/lexer_test.cc
+++ test/lexer_test.cc
@@ -1,10 +1,24 @@
 #include <gtest/gtest.h>
 #include "tiger/lexer.h"
 
-TEST(TestScanNum, HandleComplexInput) {
+#define ASSERT_TOKEN_EQ(token_ptr, token)                   \
+    do {                                                    \
+        ASSERT_NE(token_ptr, nullptr);                      \
+        ASSERT_EQ(token_ptr->Type(), token.Type());         \
+        ASSERT_EQ(token_ptr->Value(), token.Value());       \
+    } while (0);
+
+TEST(TestNum, HandleComplexInput) {
     Lexer lexer("1234");
     ASSERT_EQ(lexer.GetNextToken()->Value(), Token(Token::Tag::NUM, "1234").Value());
     ASSERT_EQ(lexer.GetNextToken(), nullptr);
+}
+
+TEST(TestComment, HandleSimpleInput) {
+    Lexer lexer("/*this is comment*/");
+    auto tokens = lexer.GetAllTokens();
+    ASSERT_EQ(tokens.size(), 1);
+    ASSERT_TOKEN_EQ(tokens.front(), Token(Token::Tag::COMMENT, "/*this is comment*/"));
 }
 
 int main(int argc, char **argv) {
