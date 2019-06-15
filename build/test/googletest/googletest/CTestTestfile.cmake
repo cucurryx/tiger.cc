4ad0eabe3905a3fc8c07717feeca9c8cb28d8fb8
xiebei <xiebei1108@outlook.com>
2019-06-15 14:56:36 +0800

    feat: pass all parser testcases

---
+++ build/test/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/xiebei/tiger.cc/test/googletest/googletest
+# Build directory: /Users/xiebei/tiger.cc/build/test/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
