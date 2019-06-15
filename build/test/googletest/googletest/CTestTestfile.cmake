892c66d0eb74d682111ae757656da237cb2e381e
xiebei <xiebei1108@outlook.com>
2019-06-15 15:02:51 +0800

    fix: remove build/

--- build/test/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/xiebei/tiger.cc/test/googletest/googletest
-# Build directory: /Users/xiebei/tiger.cc/build/test/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
